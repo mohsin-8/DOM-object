2f9775fe5714149e4bb144193962ddb38443d7f6
mohsin-8 <mohsinalikhan518@gmail.com>
2021-12-05 20:44:08 +0500

    objects

---
+++ app.js
@@ -0,0 +1,25 @@
+
+var main = document.getElementById('main-content').childNodes;
+console.log(main);
+
+// var x = document.getElementsByClassName('render').innerHTML;
+// for (var i = 0; i < x.length; i++) {
+// console.log(  x  );
+// }
+
+var firstName = document.getElementById('first-name');
+console.log(firstName.value = 'alex');
+
+var lastName = document.getElementById('last-name');
+console.log(lastName.value = 'bank');
+
+var Email = document.getElementById('email');
+console.log(Email.value = 'alexbank@example.com');
+
+
+var formContent = document.getElementById('form-content').nodeType;
+// console.log(formContent);
+// formContent.innerHTML
+
+var answer1 = document.getElementById('answerOne');
+answer1 = formContent;
